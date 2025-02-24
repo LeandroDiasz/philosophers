204c8ac0271959c71b80cb8d4fdca4cc042adbab
ledias-d <leocraftjg@gmai.com>
2025-02-24 20:04:44 -0300

    [feat] Implementar função principal com fluxo de inicialização e simulação

---
+++ main.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ledias-d <ledias-d@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-02-18 23:11:40 by ledias-d          #+#    #+#             */
+/*   Updated: 2025-02-18 23:11:40 by ledias-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+int main(int ac, char **av)
+{
+    t_table table;
+    if (ac == 5 || ac == 6)
+    {
+        parse_input(av, &table);
+        init_table(&table);
+        start_simulation(&table);
+        clean(&table);
+    }
+    else
+    {
+        error_exit("Error: Wrong number of arguments\n");
+        return (0);
+    }
+}
