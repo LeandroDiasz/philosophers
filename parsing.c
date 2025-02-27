62bef5375fa498977aae4c496539d09b55543ca9
ledias-d <leocraftjg@gmai.com>
2025-02-27 18:11:06 -0300

    feat: adicionei a função ft_atol para transformar os parametros passados, e a parse_input que vai chamar as atol

---
+++ parsing.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ledias-d <ledias-d@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-02-19 17:43:36 by ledias-d          #+#    #+#             */
+/*   Updated: 2025-02-19 17:43:36 by ledias-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+long    ft_atol(const char *str)
+{
+    long    res;
+
+    res = 0;
+    while (*str == ' ' || *str == '\t' || *str == '\n' ||
+    *str == '\v' || *str == '\f' || *str == '\r')
+        str++;
+    if (*str == '-' || *str == '+')
+    {
+        if (*str == '-')
+            error_exit("Error: Negative number of philosophers\n");
+        str++;
+    }
+    while (*str >= '0' && *str <= '9')
+    {
+        res = res * 10 + *str - '0';
+        str++;
+    }
+    if (res > INT_MAX)
+        error_exit("Error: Number of philosophers too big\n");
+    return (res);
+}
+
+void    parse_input(char **av, t_table *table)
+{
+    table->num_philos = ft_atol(av[1]);
+    table->time_to_die = ft_atol(av[2]);
+    table->time_to_eat = ft_atol(av[3]);
+    table->time_to_sleep = ft_atol(av[4]);
+    if (av[5])
+        table->nbr_limits_meals = ft_atol(av[5]);
+    else
+        table->nbr_limits_meals = -1;
+}
