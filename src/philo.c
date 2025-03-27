ea9f3a3bb23ecb7f4fb5dcbb145b3ca65d96f674
ledias-d <leocraftjg@gmai.com>
2025-03-27 11:54:32 -0300

    Refatorar: norma

--- src/philo.c
+++ src/philo.c
@@ -34,46 +34,45 @@ void	start_simulation(t_table *table)
 	pthread_join(monitor_thread, NULL);
 }
 
-void    take_forks(t_philo *philo)
+void	take_forks(t_philo *philo)
 {
 	if (verify_end(philo))
 		return ;
-    pthread_mutex_lock(&philo->left_fork->mutex);
-    ft_print_status(philo, "has taken a fork");
-    pthread_mutex_lock(&philo->right_fork->mutex);
-    ft_print_status(philo, "has taken a fork");
+	pthread_mutex_lock(&philo->left_fork->mutex);
+	ft_print_status(philo, "has taken a fork");
+	pthread_mutex_lock(&philo->right_fork->mutex);
+	ft_print_status(philo, "has taken a fork");
 }
 
-void    eat(t_philo *philo)
+void	eat(t_philo *philo)
 {
 	if (verify_end(philo))
 		return ;
-    ft_print_status(philo, "is eating");
-    ft_usleep(philo->table->time_to_eat);
+	ft_print_status(philo, "is eating");
+	ft_usleep(philo->table->time_to_eat);
 	pthread_mutex_lock(&philo->philo_mutex);
-    philo->last_meal_time = get_time();
-    philo->meals_counter++;
+	philo->last_meal_time = get_time();
+	philo->meals_counter++;
 	pthread_mutex_unlock(&philo->philo_mutex);
 }
 
-void    unlock_forks(t_philo *philo)
+void	unlock_forks(t_philo *philo)
 {
-    pthread_mutex_unlock(&philo->left_fork->mutex);
-    pthread_mutex_unlock(&philo->right_fork->mutex);
+	pthread_mutex_unlock(&philo->left_fork->mutex);
+	pthread_mutex_unlock(&philo->right_fork->mutex);
 	if (verify_end(philo))
 		return ;
 }
 
 void	*routine(void *arg)
 {
-	t_philo *philo;
+	t_philo	*philo;
 
 	philo = (t_philo *)arg;
-
 	if (philo->id % 2 == 0)
 		ft_usleep(100);
-	while ((philo->table->nbr_limits_meals == -1 
-	|| philo->meals_counter < philo->table->nbr_limits_meals))
+	while ((philo->table->nbr_limits_meals == -1
+			|| philo->meals_counter < philo->table->nbr_limits_meals))
 	{
 		if (verify_end(philo))
 			return (NULL);
@@ -83,8 +82,8 @@ void	*routine(void *arg)
 			return (NULL);
 		}
 		take_forks(philo);
-        eat(philo);
-        unlock_forks(philo);
+		eat(philo);
+		unlock_forks(philo);
 		ft_print_status(philo, "is sleeping");
 		ft_usleep(philo->table->time_to_sleep);
 		ft_print_status(philo, "is thinking");
