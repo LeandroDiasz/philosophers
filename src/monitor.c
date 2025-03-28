af6941ca7c4eba31f8d28260bcc808fea41871ba
ledias-d <leocraftjg@gmai.com>
2025-03-27 22:38:47 -0300

    Refatorar: Adicionar função ft_print_status para imprimir status dos filósofos

--- src/monitor.c
+++ src/monitor.c
@@ -19,10 +19,8 @@ void	monitor(t_table *table)
     if (table->num_philos == 1)
     {
         ft_usleep(table->time_to_die);
-		printf("%ld %d died\n", get_time() - table->start_time, table->philos[0].id);
+		ft_print_status(&table->philos[0], "died");
 		table->end_table = true;
-        table->philos[0].end_table = true;
-        pthread_mutex_unlock(&table->philos[0].right_fork->mutex);
 		return;
     }
 	while (!table->end_table)
@@ -39,24 +37,22 @@ void	monitor(t_table *table)
 int	check_philo_status(t_table *table, t_philo *philo)
 {
 	int	i;
+	long long	time;
 
 	i = 0;
-	if (philo->meals_counter == table->nbr_limits_meals)
+	if (philo->full)
 		table->full_philos++;
 	if (table->full_philos == table->num_philos)
 	{
 		table->end_table = true;
 		return (1);
 	}
-	if (get_time() - philo->last_meal_time > table->time_to_die)
+	time = get_time() - philo->last_meal_time;
+	//printf("time: %lld\n", time);
+	if (time > table->time_to_die)
 	{
-		printf("%ld %d died\n", get_time() - table->start_time, philo->id);
+		ft_print_status(philo, "died");
 		table->end_table = true;
-		while (i < table->num_philos)
-		{
-			table->philos[i].end_table = true;
-			i++;
-		}
 		return (1);
 	}
 	return (0);
